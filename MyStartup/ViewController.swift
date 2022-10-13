5948415b9adeb6e31ea115262de1562c638ff8fc
Usha <usharanin26@gmail.com>
2022-10-13 16:15:21 -0500

    Initial Commit

---
+++ MyStartup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStartup
+//
+//  Created by student on 10/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
